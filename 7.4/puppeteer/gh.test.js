c7a497326061b7fe2f15116d476c8c1126f0772c
RinaK27 <megustarina@gmail.com>
2023-05-20 21:27:53 +0400

    Initial

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -13,13 +13,13 @@ describe("Github page tests", () => {
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForSelector('h1'), {timeout:10000};
     const title2 = await page.title();
     expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
   });
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", link => link.getAttribute('href'), {timeout:10000});
     expect(actual).toEqual("#start-of-content");
   });
 
@@ -27,8 +27,53 @@ describe("Github page tests", () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
-    });
+    }, {timeout:10000});
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Sign up for free")
   });
 });
+
+  describe("Github test1", () => {
+    beforeEach(async () => {
+      page = await browser.newPage();
+      await page.goto("https://github.com/security");
+    });
+    afterEach(() => {
+      page.close();
+    });
+    test("The h1 should contain 'Trusted by millions of developers'", async () => {
+      const expected = "Trusted by millions of developers";
+      const actual = await page.$eval("h1", (link) => link.textContent);
+      expect(actual).toContain(expected);
+      });
+  });
+
+  describe("Github test2", () => {
+    beforeEach(async () => {
+      page = await browser.newPage();
+      await page.goto("https://github.com/enterprise");
+    });
+    afterEach(() => {
+      page.close();
+    });
+    test("The h1 should contain 'Build like the best'", async () => {
+      const expected = "Build like the best";
+      const actual = await page.$eval("h1", (link) => link.textContent);
+      expect(actual).toContain(expected);
+      });
+  });
+
+  describe("Github test3", () => {
+    beforeEach(async () => {
+      page = await browser.newPage();
+      await page.goto("https://github.com/features");
+    });
+    afterEach(() => {
+      page.close();
+    });
+    test("The h1 should contain 'The tools you need to build what you want.'", async () => {
+      const expected = "The tools you need to build what you want.";
+      const actual = await page.$eval("h1", (link) => link.textContent);
+      expect(actual).toContain(expected);
+      });
+  })
--- 7.4/puppeteer/jest.config.js
+++ 7.4/puppeteer/jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     verbose: true, //указывает на то, что каждый тест будет показан в отчете о процессе запуска 
      preset: "jest-puppeteer", //указываем, что будем использовать эту библиотеку
-     testTimeout: 60000
- };
+     launch: {
+        // здесь можем указывать все глобальные параметры запуска браузера для функции launch()
+        slowMo: 1000,
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized'] 
+        }
+}
